c971e5ab6b83245d6d3c97feed19f00247e1b44c
Amit Gupta <github@amitg.pro>
2024-10-15 16:59:16 -0500

    WIP

--- vue-profile/src/components/TechnicalSkills.vue
+++ vue-profile/src/components/TechnicalSkills.vue
@@ -1,102 +1,134 @@
 <template>
-    <v-card class="text-left">
-      <v-card-title class="capitalize">TECHNICAL SKILLS</v-card-title>
-      <v-card-text>
-        <table border="1" cellpadding="10" cellspacing="0">
-    <thead>
-        <tr>
-            <th>Category</th>
-            <th>Skills</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Architectural patterns</td>
-            <td>MVC, MVVM, MACH</td>
-        </tr>
-        <tr>
-            <td>UI Development</td>
-            <td>TypeScript, JavaScript, HTML5, CSS3, Angular(v2,4,6,8,12,16), React, VueJs(v2.4,2.7,3), Svelte, Remix, Nuxt, Next.js, Sass, Less, Tailwind, Bootstrap, PHP (v3,4,5), JSP, ASP, Tcl/Tk, Qt</td>
-        </tr>
-        <tr>
-            <td>UX Prototyping</td>
-            <td>Sketch, Figma, Adobe XD, LucidDraw</td>
-        </tr>
-        <tr>
-            <td>Backend development</td>
-            <td>Python(v2.4,v2.7,v3), Node(v12-22), Deno, Golang, Rust</td>
-        </tr>
-        <tr>
-            <td>System Integrations</td>
-            <td>REST, RPC, gRPC, SOAP, IPC, Electronic Data Interchange(EDI)</td>
-        </tr>
-        <tr>
-            <td>AuthTech</td>
-            <td>OpenID Connect, OAuth2, FIDO, PassKeys</td>
-        </tr>
-        <tr>
-            <td>Build tools and code packaging</td>
-            <td>Rollup, Webpack, Make, Grunt, Gulp, Snaps, deb, rpms</td>
-        </tr>
-        <tr>
-            <td>Infrastructure as Code</td>
-            <td>Terraform, Puppet, Chef, Jenkins, GitHub Actions, Azure Pipelines, CircleCI, CloudFormation</td>
-        </tr>
-        <tr>
-            <td>Mobile app development</td>
-            <td>Ionic, Cordova, Capacitor, Flutter, React Native, Phonegap</td>
-        </tr>
-        <tr>
-            <td>Cloud platforms</td>
-            <td>AWS, Azure , GCP, Digital Ocean</td>
-        </tr>
-        <tr>
-            <td>Cluster management</td>
-            <td>Kubernetes, Docker Swarm, Flowtracer, LSF</td>
-        </tr>
-        <tr>
-            <td>Project Management</td>
-            <td>Jira, Rally, Azure Boards, Asana</td>
-        </tr>
-        <tr>
-            <td>Data Management</td>
-            <td>MySQL, PostgreSQL, SQLite, Apache Cassandra</td>
-        </tr>
-        <tr>
-            <td>Big Data</td>
-            <td>Apache Hadoop, Spark, Flume, Datalake, data mesh, Elastic Search, PowerBI, Tableau, Sumologic</td>
-        </tr>
-        <tr>
-            <td>Version Control Systems</td>
-            <td>Git (GitHub Enterprise, Azure Repos, Bitbucket, Gitlab), Perforce, DesignSync</td>
-        </tr>
-        <tr>
-            <td>Generative AI</td>
-            <td>OpenAI, Claude, Pi, Gemini</td>
-        </tr>
-        <tr>
-            <td>IoT systems</td>
-            <td>AWS IoT, Google IoT, Azure IoT, BLE, MQTT, LoRaWAN, Zigbee, Kafka</td>
-        </tr>
-        <tr>
-            <td>Natural Language Processing</td>
-            <td>Azure AI Language , Azure AI Speech, Azure AI Translator</td>
-        </tr>
-    </tbody>
-</table>
-      </v-card-text>
-    </v-card>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        
-      }),
-    }
+  <v-card
+    title="TECHNICAL SKILLS"
+class="text-left"
+  >
+      <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    item-key="skills"
+    items-per-page="-1"
+    :search="search"
+  ></v-data-table>
+  </v-card>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const search = ref('')
   </script>
-  <style scoped>
-  
-.capitalize {
-  text-transform: capitalize !important;
+<script>
+export default {
+  data() {
+    return {
+      search: '',
+      headers: [
+        { text: 'Category', value: 'category' },
+        { text: 'Skills', value: 'skills' }
+      ],
+      items: [
+    {
+      "category": "Architectural patterns",
+      "skills": "MVC MVVM MACH"
+    },
+    {
+      "category": "UI Development",
+      "skills": 
+        "TypeScript, JavaScript, HTML5, CSS3, Angular(v2,4,6,8,12,16), React, VueJs(v2.4,2.7,3), Svelte, Remix, Nuxt, Next.js, Sass, Less, Tailwind, Bootstrap, PHP (v3,4,5), JSP, ASP, Tcl/Tk, Qt"
+      
+    },
+    {
+      "category": "UX Prototyping",
+      "skills":"Sketch, Figma, Adobe XD, LucidDraw"
+    },
+    {
+      "category": "Backend development",
+      "skills":"Python(v2.4,v2.7,v3), Node(v12-22), Deno, Golang, Rust"
+    },
+    {
+      "category": "System Integrations",
+      "skills":"REST, RPC, gRPC, SOAP, IPC, Electronic Data Interchange(EDI)"
+    },
+    {
+      "category": "AuthTech",
+      "skills":"OpenID Connect, OAuth2, FIDO, PassKeys"
+    },
+    {
+      "category": "Build tools and code packaging",
+      "skills":"Rollup, Webpack, Make, Grunt, Gulp, Snaps, deb, rpms"
+    },
+    {
+      "category": "Infrastructure as Code",
+      "skills": 
+        "Terraform, Puppet, Chef, Jenkins, GitHub Actions, Azure Pipelines, CircleCI, CloudFormation"
+      
+    },
+    {
+      "category": "Mobile app development",
+      "skills":"Ionic, Cordova, Capacitor, Flutter, React Native, Phonegap"
+    },
+    {
+      "category": "Cloud platforms",
+      "skills":"AWS, Azure, GCP, Digital Ocean"
+    },
+    {
+      "category": "Cluster management",
+      "skills":"Kubernetes, Docker Swarm, Flowtracer, LSF"
+    },
+    {
+      "category": "Project Management",
+      "skills":"Jira, Rally, Azure Boards, Asana"
+    },
+    {
+      "category": "Data Management",
+      "skills":"MySQL, PostgreSQL, SQLite, Apache Cassandra"
+    },
+    {
+      "category": "Big Data",
+      "skills": 
+        "Apache Hadoop, Spark, Flume, Datalake, data mesh, Elastic Search, PowerBI, Tableau, Sumologic"
+      
+    },
+    {
+      "category": "Version Control Systems",
+      "skills": 
+        "Git (GitHub Enterprise, Azure Repos, Bitbucket, Gitlab), Perforce, DesignSync"
+      
+    },
+    {
+      "category": "Generative AI",
+      "skills":"OpenAI, Claude, Pi, Gemini"
+    },
+    {
+      "category": "IoT systems",
+      "skills": 
+        "AWS IoT, Google IoT, Azure IoT, BLE, MQTT, LoRaWAN, Zigbee, Kafka"
+      
+    },
+    {
+      "category": "Natural Language Processing",
+      "skills":"Azure AI Language,Azure AI Speech, Azure AI Translator"
+    }
+  ]
 }
-  </style>
+
+  }
+};
+</script>
+
+<style scoped>
+.v-card-title {
+  font-weight: bold;
+}
+</style>
